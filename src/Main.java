2ffa065ab72ce4686b3034c500b9970e1a585d01
tony24123 <tjdduswns01@naver.com>
2024-11-20 10:41:29 +0900

    세번째 커밋

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(10 * 3);
         System.out.println(10);
-        System.out.println(20);
+        System.out.println(2024);
     }
 }
